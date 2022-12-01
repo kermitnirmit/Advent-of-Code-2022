b38140efab1f7d1513591c92d63da7163990b42f
Nirmit Shah <nirmit@squareup.com>
2022-12-01 00:21:53 -0500

    even cleaner

--- day_01/solution.py
+++ day_01/solution.py
@@ -1,4 +1,3 @@
-f = open("input.txt").read().strip().split("\n\n")
-counts = sorted([sum(int(x) for x in y.split("\n")) for y in f])
-print(max(counts))
-print(sum(counts[-3:]))
+f = sorted([sum(int(x) for x in y.split("\n")) for y in open("input.txt").read().strip().split("\n\n")])
+print(max(f))
+print(sum(f[-3:]))
