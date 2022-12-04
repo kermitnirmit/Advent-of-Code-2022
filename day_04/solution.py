4911b1de134413113b873aa9ec25d68f4dc69624
Nirmit Shah <nirmit@squareup.com>
2022-12-04 00:22:39 -0500

    ty mark

--- day_04/solution.py
+++ day_04/solution.py
@@ -1,14 +1,8 @@
-import string
+from parse import parse
 f = open("input.txt").read().strip().split("\n")
 c = d = 0
 for line in f:
-    l,r = line.split(",")
-    q,w = l.split("-")
-    q = int(q)
-    w = int(w)
-    e,r = r.split("-")
-    e = int(e)
-    r = int(r)
+    q,w,e,r = parse("{:d}-{:d},{:d}-{:d}", line)
     if q <= e <= r <= w or e <= q <= w <= r:
         c += 1
     if max(q,e) <= min(w,r):
