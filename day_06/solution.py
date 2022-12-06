5986733377589b654150170df20f70d93847aaca
Nirmit Shah <nirmit@squareup.com>
2022-12-06 00:06:31 -0500

    wow that was easy

---
+++ day_06/solution.py
@@ -0,0 +1,7 @@
+f = open("input.txt").read().strip()
+for q in range(2):
+    gap = 14 if q == 1 else 4
+    for i in range(0, len(f) - gap):
+        if len(set(f[i:i+gap])) == gap:
+            print(i + gap)
+            break
