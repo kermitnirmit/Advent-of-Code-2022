5ff0f11f93deef3b8079fd6fc4f97b00898eadfd
Nirmit Shah <nirmit@squareup.com>
2022-12-13 00:58:59 -0500

    day 13

---
+++ day_13/solution.py
@@ -0,0 +1,52 @@
+import functools
+import ast
+
+f = open("input.txt").read().strip().split("\n\n")
+
+
+def compare(l, r):
+    if type(l) == type(r):
+        if type(l) == int:
+            if l == r:
+                return "k"
+            else:
+                return l < r
+        else:
+            for l2, r2 in zip(l, r):
+                ok = compare(l2, r2)
+                if not ok:
+                    return False
+                if ok is True:
+                    return True
+            if len(l) < len(r):
+                return True
+            if len(r) < len(l):
+                return False
+        return "k"
+    else:
+        if type(r) == list:
+            return compare([l], r)
+        if type(l) == list:
+            return compare(l, [r])
+
+
+def customComp(x, y):
+    return - 1 * compare(x, y)
+
+
+valid = 0
+all_lines = []
+for i, pair in enumerate(f):
+    one, two = pair.split("\n")
+    one = ast.literal_eval(one)
+    two = ast.literal_eval(two)
+    all_lines.append(one)
+    all_lines.append(two)
+    if compare(one, two):
+        valid += (i + 1)
+all_lines.append([[2]])
+all_lines.append([[6]])
+
+al = sorted(all_lines, key=functools.cmp_to_key(customComp))
+print(valid)
+print((al.index([[2]]) + 1) * (al.index([[6]]) + 1))
