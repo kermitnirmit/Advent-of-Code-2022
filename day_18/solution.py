b9898c163f1c9de407194fd3e52994032932f7bb
Nirmit Shah <nirmit@squareup.com>
2022-12-18 00:46:07 -0500

    day 18

---
+++ day_18/solution.py
@@ -0,0 +1,39 @@
+import time
+
+from tqdm import trange
+from  utils import ints, neighbors_3d, _neighbors_3d
+from collections import defaultdict, Counter
+
+f = [x for x in open("input.txt").read().strip().split("\n")]
+# f = [x for x in open("test.txt").read().strip().split("\n")]
+
+
+points = set()
+
+for line in f:
+    x,y,z = ints(line)
+    points.add((x,y,z))
+
+print(sum(6 - sum(q in points for q in _neighbors_3d(point)) for point in points))
+knownout = set()
+trapped = set()
+
+def getout(point):
+    if point in knownout:
+        return True
+    if point in trapped:
+        return False
+    s = set()
+    q = [point]
+    while q:
+        curr = q.pop(0)
+        if curr not in points and curr not in s:
+            s.add(curr)
+            if len(s) > 2000: #searching a ton and couldnt get stuck inside
+                knownout.update(s)
+                return True
+            q.extend(_neighbors_3d(curr))
+    # if queue's done, we've seen things that are trapped
+    trapped.update(s)
+    return False
+print(sum(sum(getout(q) for q in _neighbors_3d(point)) for point in points))
